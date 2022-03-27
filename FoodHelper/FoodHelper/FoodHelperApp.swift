2e18aa6dceeb1a18c67da2cf7e9eddddbfe96e3a
perezryan26 <Perezryan26@gmail.com>
2022-03-26 22:05:03 -0500

    Created File ...

---
+++ FoodHelper/FoodHelper/FoodHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodHelperApp.swift
+//  FoodHelper
+//
+//  Created by Ryan Perez on 3/26/22.
+//
+
+import SwiftUI
+
+@main
+struct FoodHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
